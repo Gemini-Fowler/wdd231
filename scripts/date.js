fd9904a4c046682a61dd08c52219ad19d5647d87
Jonas Crothers <jonascrothers@gmail.com>
2025-03-08 01:05:33 -0800

    dingo

---
+++ scripts/date.js
@@ -0,0 +1,5 @@
+// Set the current year dynamically
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set the document last modified date
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
